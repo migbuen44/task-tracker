8f6eb49db7f6e53a6c2c4f489607417887c9f557
Miguel Buenviaje <migbuen44@gmail.com>
2021-12-06 21:47:17 -0800

    style(Login): create general layout for Login page

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,10 +1,15 @@
 import styles from './header.module.scss'
+import Link from 'next/link'
 
 const HeaderMain = () => {
 
   return (
     <div className={styles.container}>
-
+      <span className={styles.login_button_container}>
+        <Link href="/login" passHref>
+          <button className={styles.logn_button}>Login</button>
+        </Link>
+      </span>
     </div>
   )
 }
