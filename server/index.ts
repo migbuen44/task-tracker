89e3896c7f33397ac09dc2b048f306ae77f3356a
Miguel Buenviaje <migbuen44@gmail.com>
2021-12-24 17:22:58 -0800

    chore(server): Add router file and controllers folder

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
+import router from './router'
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cors());
 app.use(morgan('dev'));
+app.use(router);
 
 app.get('/', (req, res) => {
   res.json('Hello World!');
